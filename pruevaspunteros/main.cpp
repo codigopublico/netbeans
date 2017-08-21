9cb126cdd6ccdad1c7775e9300f22b399cf24bfe
prog <prog@classe.net>
2017-08-21 14:05:29 +0200

    while

--- pruevaspunteros/main.cpp
+++ pruevaspunteros/main.cpp
@@ -46,8 +46,7 @@ void p1(){
 void p2(){
     char *dia[7] = { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
     for(int i = 0; i < 7; i++){
-        *++dia;
-    imprime( *dia);
+    imprime( *dia[i]);
     }
 }
 int main(int argc, char** argv) {
