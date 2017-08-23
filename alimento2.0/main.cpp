2fe8f13fa92ed040d68b52f262dcb5990fa34acb
prog <prog@classe.net>
2017-08-23 14:46:00 +0200

    while

--- alimento2.0/main.cpp
+++ alimento2.0/main.cpp
@@ -6,12 +6,87 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
  * 
  */
+class alimentos{
+public:
+    int estad[0][2];
+    int estad[0][1] = 1;
+    //propiedades del alimento
+    int prot;
+    int gras;
+    int hidra;
+    int cali;
+    void frie(){
+        
+    }
+    void asa(){
+        
+    }
+    void come(){
+        
+    }
+    void pela(){
+       
+    }
+    void estado(){
+        int i = 1;
+        std::cout << "Tiene " << prot << " proteinas \n";
+        std::cout << "La patata esta " << estado[0][1] << "\n";
+    }
+private:
+    int aux;
+};
+class Gisantes : public alimentos{
+
+};
+class carnes : public alimentos{
+public:
+    int prot = 0;
+    int gras = 0;
+    int hidra = 0;
+    int cali = 0;
+};
+class buey : public carnes{
+    
+};
+class pescados : public alimentos{
+public:
+    int prot = 0;
+    int gras = 0;
+    int hidra = 0;
+    int cali = 0;
+};
+class merluza : public pescados{
+    
+};
+class salmon : public pescados{
+    
+};
+class atun : public pescados{
+    
+};
+class verduras : public alimentos{
+public:
+    int prot = 0;
+    int gras = 0;
+    int hidra = 0;
+    int cali = 0;
+};
+class judias : public verduras{
+    
+};
+class espinacas : public verduras{
+    
+};
+class ternera : public carnes{
+    
+};
 int main(int argc, char** argv) {
 
     return 0;
