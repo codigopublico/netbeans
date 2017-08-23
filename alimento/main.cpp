6923799ebe82661527766cf17f5f127b1f0040f3
prog <prog@classe.net>
2017-08-23 12:54:40 +0200

    while

---
+++ alimento/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: prog
+ *
+ * Created on 23 de agosto de 2017, 12:53
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
