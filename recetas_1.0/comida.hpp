f51624a024069a9e6e82d3656cb4f3b9c4009bca
prog <prog@classe.net>
2017-08-23 16:54:27 +0200

    while

--- recetas_1.0/comida.hpp
+++ recetas_1.0/comida.hpp
@@ -11,4 +11,179 @@
 
 
 #endif	/* COMIDA_HPP */
+#include <cstdlib>
+#include <string>
+#include <iostream>
+using namespace std;
 
+/*
+ * 
+ */
+void impestados(int a){
+    string estado[] ={"frito", "asado", "pelado", "comido", "crudo" };
+    std::cout << "El alimento esta " << estado[a] << "\n";
+}
+class alimentos{
+public:
+    int aux[4];
+    int ii;
+    //propiedades del alimento
+    int prot;
+    int gras;
+    int hidra;
+    int cali;
+    void frie(){
+        aux[0] = 1;
+    }
+    void asa(){
+        aux[1] = 1;
+    }
+    void come(){
+        aux[2] = 1;
+    }
+    void pela(){
+        aux[3] = 1;
+    }
+    void estados(){
+        std::cout << "Tiene " << prot << " proteinas \n";
+        ii = 0;
+        for(int i = 0; i < 4; i++){
+            if(aux[i] == 1){
+                impestados(i);
+            }else{
+                ii++;
+            }
+            if(ii == 4){
+                impestados(ii);
+                
+            }
+        }
+    }
+    void cal(){
+        return cal;
+    }
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+
+};
+class Gisantes : public alimentos{
+
+};
+class carnes : public alimentos{
+public:
+    void iniciar(){
+    int prot = 0;
+    int gras = 0;
+    int hidra = 0;
+    int cali = 0;
+    }
+};
+class buey : public carnes{
+    
+};
+class pescados : public alimentos{
+public:
+    void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 10;
+   }
+};
+class merluza : public pescados{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class salmon : public pescados{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class atun : public pescados{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class verduras : public alimentos{
+public:
+    void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class judias : public verduras{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 5S;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class espinacas : public verduras{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class ternera : public carnes{
+     void iniciar(){
+    prot = 0;
+    gras = 0;
+    hidra = 0;
+    cali = 0;
+    for(int i = 0; i < 5; i++) {
+        aux[i] = 0;
+    }
+    }
+};
+class cerdo : public carnes{
+    
+};
+class sardina : public pescados{
+    
+};
+class frutas : public alimentos{
+    
+};
+class Kiwi : public frutas{
+    
+};
--- recetas_1.0/main.cpp
+++ recetas_1.0/main.cpp
@@ -6,14 +6,38 @@
  */
 
 #include <cstdlib>
+#include "comida.hpp"
 
 using namespace std;
 
 /*
  * 
  */
+class recetas : public alimentos{
+public:
+    void ingredientes(int c){
+        aux[aux] = c;
+        aux++;
+        std::cout << "  " << aux << "  " << cali;
+    }
+    void personas(int a){
+       
+    }
+    void Tiempo(){
+        
+    }   
+private:
+    int aux = 0;
+    int aux2[9];
+};
 int main(int argc, char** argv) {
-
+    pescados salmon;
+    salmon.iniciar();
+    verduras lechuga;
+    lechuga.iniciar();
+    recetas gazpacho();
+    gazpacho.ingredientes(salmon.cal());
+    gazpacho.ingredientes(lechuga.cal());
     return 0;
 }
 
