18685653e10862a9bcfafe242ef78d18e844edfe
prog <prog@classe.net>
2017-08-24 16:38:11 +0200

    while

--- recetas_1.0/main.cpp
+++ recetas_1.0/main.cpp
@@ -72,7 +72,7 @@ void p2(){
     std::cout << "\n";
     int i = 0;
     for(int ii = 0; a[ii] !=0; ii++){
-        std::cin >> a[i];
+        std::cin >> a[ii];
         i++;
     }
     for(int ii = 0; ii < i; ii++){
