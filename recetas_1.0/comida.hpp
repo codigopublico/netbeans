4469a92d27f84219411d221cd22cc681066bccab
prog <prog@classe.net>
2017-08-24 09:28:54 +0200

    while

--- recetas_1.0/comida.hpp
+++ recetas_1.0/comida.hpp
@@ -24,6 +24,7 @@ void impestados(int a){
     std::cout << "El alimento esta " << estado[a] << "\n";
 }
 string alimento[] ={"carnes", "pescados", "merluza", "salmon", "atun", "buey", "verduras"};
+int r[2];
 class alimentos{
 public:
     int aux[4];
@@ -62,7 +63,6 @@ public:
         }
     }
     int cal(){
-        int r[2];
         r[0] = cali;
         r[1] = id;
         int *p;
