8ff43cad9d7648e49ebbfb00e4162f1488338bbc
prog <prog@classe.net>
2017-08-25 13:52:50 +0200

    while

--- ejemplo_de_retorno_punteros/main.cpp
+++ ejemplo_de_retorno_punteros/main.cpp
@@ -6,14 +6,26 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
 /*
- * 
+ * Esto sirve como resolver el tema de los head los punteros i demas.
  */
+int* retorno(int r[]){
+    int *p = new int[6];
+    p = r;
+    return p;
+}
 int main(int argc, char** argv) {
-
+    int r[] = {1, 2, 3, 4, 5, 6};
+    int *e = retorno(r); 
+    int v[6];
+    for(int i = 0; i < 6; i++){
+        v[i] = *e;
+    std::cout << v[i] << "\n";
+     e++;
+    }
     return 0;
 }
 
